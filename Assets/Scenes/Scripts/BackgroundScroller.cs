68562fb45002e931ebda67d1d604c6f1a823b768
Mykhailo Zhovnach <mykhailo.zhovnach@gmail.com>
2019-01-26 16:19:02 +0200

    Initial Unity Project

---
+++ Assets/Scenes/Scripts/BackgroundScroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+
+    private float _speed;
+    public Material Material;
+
+	// Use this for initialization
+	void Start () {
+        _speed = 10.5f;
+	}
+
+    // minus speed means scroll in opposite direction
+    public void SetScrollSpeed(float speed)
+    {
+        float offset = Time.time * _speed;
+        Material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+	
+	// Update is called once per frame
+	public void Update () {
+		
+	}
+}
---
+++ Assets/Scenes/Scripts/GameBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour {
+
+
+    public BackgroundScroller BackgroundScroller;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
